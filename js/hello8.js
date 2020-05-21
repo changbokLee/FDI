9310fb314681c0ca25d2e380ade371b6b86134e8
changbok <changbok2453@gmail.com>
2020-05-21 17:46:21 +0900

    자스공부

--- js/hello8.js
+++ js/hello8.js
@@ -7,24 +7,52 @@
 // var result = menu[menuNum];
 // document.write(result);
 
-var userEmail = prompt("이메일을 입력하세요");
-var arrUrl = ["co.kr" , ".com" , ".net" ,  "or.kr", "go.kr"];
-var check1 = false;
-var check2 = false;
+// var userEmail = prompt("이메일을 입력하세요");
+// var arrUrl = ["co.kr" , ".com" , ".net" ,  "or.kr", "go.kr"];
+// var check1 = false;
+// var check2 = false;
 
-if (userEmail.indexOf("@") > 0)
-    check1 = true;
+// if (userEmail.indexOf("@") > 0)
+//     check1 = true;
     
 
-for(var i = 0 ; i < arrUrl.length; i ++)
-    {
-        if (useremail.indexOf(arrUrl[i] > 0 )) 
-            check2 =true;
+// for(var i = 0 ; i < arrUrl.length; i ++)
+//     {
+//         if (useremail.indexOf(arrUrl[i] > 0 )) 
+//             check2 =true;
             
-    }
+//     }
 
-if(check1 == ture && check2 == true)
-    document.write(userEmail);
+// if(check1 == ture && check2 == true)
+//     document.write(userEmail);
 
-else
-    document.write("바르게입력하세요");
+// else
+//     document.write("바르게입력하세요");
+
+// window.open("http://daum.net" , "daum" , "width =600" , height = "200", )
+// var scr = screen;
+// var sc_w = scr.width;
+// var sc_h = scr.height;
+
+// document.write("모니터 해상도 너비" + sc_w , "<br>");
+// document.write("모니터 해상도 높이" + sc_h , "<br>");
+
+// var count = 0;
+// function myFuntion(){
+//     document.write("안녕하세요");
+// }
+
+// myFuntion();
+// myFuntion();
+
+// var color = ["white" , "yellow", " aqua" , "purple"];
+// var i = 0;
+// function change(){
+    
+//     i++;
+//     if(i > color.length)
+//         i = 0;
+
+// }
+
+
